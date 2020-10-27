28649dc2e733000eedd2f6b0afb6d87f458f6eab
rafaelngp <rafaelferreira.dev3@gmail.com>
2020-10-26 21:33:56 -0300

    commit inicial

---
+++ src/test/java/com/finalcase/oaks/OaksFinalCase/OaksFinalCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalcase.oaks.OaksFinalCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaksFinalCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

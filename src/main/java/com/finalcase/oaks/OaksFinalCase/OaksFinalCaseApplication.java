c1c7d962b16c358f61057ae00ea624cacfd0b8cf
Rafael Ferreira <Rafaelferreira.dev3@gmail.com>
2020-10-27 13:57:04 -0300

    alteracao no application.yml
    necessario para fazer a aplicacao subir, podemos customizar os valores mais tarde

--- src/main/java/com/finalcase/oaks/OaksFinalCase/OaksFinalCaseApplication.java
+++ src/main/java/com/finalcase/oaks/OaksFinalCase/OaksFinalCaseApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OaksFinalCaseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OaksFinalCaseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OaksFinalCaseApplication.class, args);
+    }
 
 }

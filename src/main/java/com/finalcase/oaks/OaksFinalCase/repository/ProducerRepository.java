a3d461f895ea70f810f6fbec222f7baf5746069e
spina <juliana.s.lunguinho@hotmail.com>
2020-10-27 20:55:22 -0300

    Alteração ProducerRepository

--- src/main/java/com/finalcase/oaks/OaksFinalCase/repository/ProducerRepository.java
+++ src/main/java/com/finalcase/oaks/OaksFinalCase/repository/ProducerRepository.java
@@ -1,11 +1,7 @@
 package com.finalcase.oaks.OaksFinalCase.repository;
 
-import com.finalcase.oaks.OaksFinalCase.entity.Actor;
+import com.finalcase.oaks.OaksFinalCase.entity.Producer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface ProducerRepository extends JpaRepository<Actor, Integer> {
-
-//    List<Actor> findByMerchant(String merchant);
+public interface ProducerRepository extends JpaRepository<Producer, Integer> {
 }

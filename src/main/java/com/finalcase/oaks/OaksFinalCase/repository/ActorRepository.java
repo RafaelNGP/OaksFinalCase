e2ae3fe85d5745fd5f479483864220298c0f8cc5
Rafael Ferreira <Rafaelferreira.dev3@gmail.com>
2020-10-27 22:00:04 -0300

    adicionado priceLessThan

--- src/main/java/com/finalcase/oaks/OaksFinalCase/repository/ActorRepository.java
+++ src/main/java/com/finalcase/oaks/OaksFinalCase/repository/ActorRepository.java
@@ -11,4 +11,5 @@ public interface ActorRepository extends JpaRepository<Actor, Integer> {
     List<Actor> findAllByGenre(String genre);
     List<Actor> findByStartDateAfter(Date date);
     List<Actor> findByStartDateBetween(Date date1, Date date2);
+    List<Actor> findByPriceLessThan(double price);
 }

1b3ce56aea27cca2504d711a14233ff61a3ce056
elderleelfs <elderlee2009@hotmail.com>
2020-10-27 21:32:05 -0300

    ADD getbyId

--- src/main/java/com/finalcase/oaks/OaksFinalCase/service/ActorService.java
+++ src/main/java/com/finalcase/oaks/OaksFinalCase/service/ActorService.java
@@ -1,7 +1,9 @@
 package com.finalcase.oaks.OaksFinalCase.service;
 
 import com.finalcase.oaks.OaksFinalCase.entity.Actor;
+import com.finalcase.oaks.OaksFinalCase.entity.Producer;
 import com.finalcase.oaks.OaksFinalCase.repository.ActorRepository;
+import com.finalcase.oaks.OaksFinalCase.repository.ProducerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +12,15 @@ public class ActorService {
 
     @Autowired
     private ActorRepository actorRepository;
+    ActorRepository repository;
 
-    public Actor getActor(){
-        return actorRepository.getOne(1);
+    public Producer getById(Integer id) {
+        return repository.findById(id).orElseThrow();
     }
 
-    public void saveActor(Actor actor){
-        actorRepository.save(actor);
+    public Actor saveNewActor(String name, String doc) {
+        Actor actor = new Actor(name, doc);
+        return repository.save(actor);
     }
+
 }

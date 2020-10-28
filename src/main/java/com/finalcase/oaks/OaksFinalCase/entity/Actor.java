4d9d7c303afc246328324dc3804107ae86de0854
spina <juliana.s.lunguinho@hotmail.com>
2020-10-27 22:30:40 -0300

    Métodos do producerService

--- src/main/java/com/finalcase/oaks/OaksFinalCase/entity/Actor.java
+++ src/main/java/com/finalcase/oaks/OaksFinalCase/entity/Actor.java
@@ -4,7 +4,6 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@MappedSuperclass
 public class Actor extends Profile {
     @Id
     @GeneratedValue
--- src/main/java/com/finalcase/oaks/OaksFinalCase/entity/Producer.java
+++ src/main/java/com/finalcase/oaks/OaksFinalCase/entity/Producer.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@MappedSuperclass
 public class Producer extends Profile {
 
     @Id
